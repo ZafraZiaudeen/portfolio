e17217bddff27b8b043077bbb95a2692b91f0d58
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-10-11 22:12:10 +0530

    made small change in main

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RootLayout from './layouts/root.layout';
 import MainLayout from './layouts/main.layout';
-import Home from './pages/home.page.jsx'; 
-import AboutPage from './pages/about.page.jsx'; 
-import ProjectsPage from './pages/project.page.jsx';  
-import ProjectDetailPage from './pages/projectDetail.page.jsx'; 
-import ContactPage from './pages/contact.page.jsx';
+import HomePage from './pages/home.page';
+import AboutPage from './pages/About.page';
+import ProjectDetailPage from './pages/ProjectDetail.page';
+import ProjectsPage from './pages/Project.page';
+import ContactPage from './pages/contact.page';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<RootLayout />}>
           <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
+          <Route path='/' element={<HomePage/>} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/projects" element={<ProjectsPage />} />
             <Route path="/projects/:id" element={<ProjectDetailPage />} />
