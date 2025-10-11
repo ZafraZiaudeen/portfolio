cb06226fad59c860f89a4ee6b007a0e431dc2498
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-10-11 22:28:51 +0530

    fixed extension in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RootLayout from './layouts/root.layout';
-import MainLayout from './layouts/main.layout';
-import HomePage from './pages/home.page';
-import AboutPage from './pages/about.page';
-import ProjectDetailPage from './pages/projectDetail.page';
-import ProjectsPage from './pages/project.page';
-import ContactPage from './pages/contact.page';
+import RootLayout from './layouts/root.layout.jsx';
+import MainLayout from './layouts/main.layout.jsx';
+import HomePage from './pages/home.page.jsx';
+import AboutPage from './pages/about.page.jsx';
+import ProjectDetailPage from './pages/projectDetail.page.jsx';
+import ProjectsPage from './pages/project.page.jsx';
+import ContactPage from './pages/contact.page.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
