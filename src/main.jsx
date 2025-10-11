a09a7aa28e912506b1dee9e1dce4339f3171abfa
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-10-11 23:05:27 +0530

    corrected the path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RootLayout from './layouts/root.layout.jsx';
 import MainLayout from './layouts/main.layout.jsx';
-import HomePage from '@/pages/home.page.jsx';
+import HomePage from './pages/home.page.jsx';
 import AboutPage from './pages/about.page.jsx';
 import ProjectDetailPage from './pages/projectDetail.page.jsx';
 import ProjectsPage from './pages/project.page.jsx';
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<RootLayout />}>
           <Route element={<MainLayout />}>
-          <Route path='/' element={<HomePage/>} />
+          <Route path="/" element={<HomePage/>} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/projects" element={<ProjectsPage />} />
             <Route path="/projects/:id" element={<ProjectDetailPage />} />
